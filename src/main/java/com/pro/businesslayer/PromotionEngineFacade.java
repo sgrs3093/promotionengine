ac4b80966326396be383a67ee15a1db9cf4c89db
sagar shukla <manishashukla1092@gmail.com>
2021-07-02 20:40:53 +0530

    initial commit

--- src/main/java/com/pro/businesslayer/PromotionEngineFacade.java
+++ src/main/java/com/pro/businesslayer/PromotionEngineFacade.java
@@ -1,5 +1,99 @@
 package com.pro.businesslayer;
 
-public class PromotionEngineFacade {
+import java.util.List;
 
+import com.pro.service.Logger;
+import com.pro.service.PromotionServiceImpl;
+
+public class PromotionEngineFacade {
+	
+	Logger logger = LoggerFactory.getLogger(PromotionServiceImpl.class);
+	
+	IPromotionInputOutput consoleLayer;
+    IPromotionService promotionService;        
+    IRepository configManagement;
+
+    List<ProductCheckout> checkoutList;
+    AppliedOffer appliedOffer;
+
+    public Facade()
+    {
+        consoleLayer = new ConsoleLayer();
+        promotionService = new PromotionService();
+    }
+
+    public boolean CheckoutProducts()
+    {
+        try
+        {
+            checkoutList = consoleLayer.LoadUserInput();
+            return true;
+        }
+        catch (Exception ex)
+        {
+            LogWriter.LogWrite("Error in Checking out Products :" + ex.Message);
+        }
+        return false;
+    }
+
+    internal boolean ApplyPromotion()
+    {
+        try
+        {
+            appliedOffer = promotionService.ApplyPromotion(checkoutList, GetProductOffers());
+            return true;
+        }
+        catch (Exception ex)
+        {
+            LogWriter.LogWrite("Error in  Applying Promotio :" + ex.Message);
+        }
+        return false;
+    }
+
+    public boolean DisplayTotalPrice()
+    {
+        try
+        {
+            if (appliedOffer.Checkouts != null)
+            {
+                consoleLayer.DisplayTotalPrice(appliedOffer);
+                return true;
+            }
+        }
+        catch (Exception ex)
+        {
+            LogWriter.LogWrite("Error in  Displaying TotalPrice:" + ex.Message);
+        }
+
+        return false;
+    }
+    public List<Promotion> GetProductOffers()
+    {
+        try
+        {
+            configManagement = new ConfigRepository();
+            return configManagement.GetProductOffers();
+        }
+        catch (Exception ex)
+        {
+            LogWriter.LogWrite("Error in Getting Product Offers :" + ex.Message);
+        }
+        return new List<Promotion>();
+    }
+
+    public List<Product> GetAvilableProducts()
+    {
+        try
+        {
+            configManagement = new ConfigRepository();
+            return configManagement.GetAvilableProducts();
+        }
+        catch (Exception ex)
+        {
+            LogWriter.LogWrite("Error in Getting AvilableProducts :" + ex.Message);
+
+        }
+        return new List<Product>();
+    }
+}
 }
--- src/main/java/com/pro/service/PromotionServiceImpl.java
+++ src/main/java/com/pro/service/PromotionServiceImpl.java
@@ -2,4 +2,42 @@ package com.pro.service;
 
 public class PromotionServiceImpl {
 
+	Logger logger = LoggerFactory.getLogger(PromotionServiceImpl.class);
+	
+	public AppliedOffer ApplyPromotion(List<ProductCheckout> checkoutList, List<Promotion> promotions)
+    {
+        AppliedOffer appliedOffer = new AppliedOffer();
+
+        //Here Strategy pattern allows a  to choose an algorithm from a family of Promotion algorithms 
+        List<IPromotionStrategy> strategies = new List<IPromotionStrategy>();
+        strategies.Add(new AdditionalItemOffer());
+        strategies.Add(new ComboOffer());
+
+        try
+        {
+            foreach (ProductCheckout item in checkoutList)
+            {
+                if (item.Quantity > 0)
+                {
+                    foreach (var strategy in strategies)
+                    {
+                        if (strategy.CanExecute(item, promotions))
+                        {
+                            item.HasOffer = true;
+                            item.FinalPrice = strategy.CalculateProductPrice(checkoutList);
+                            appliedOffer.TotalPrice += item.FinalPrice;
+                            break;
+                        }
+                    }
+                }
+            }
+            appliedOffer.Checkouts = checkoutList;
+        }
+        catch (Exception ex)
+        {
+        	logger.log("Error in Applying Promotion in PromotionStrategy:" + ex);
+        }
+
+        return appliedOffer;
+    }
 }
